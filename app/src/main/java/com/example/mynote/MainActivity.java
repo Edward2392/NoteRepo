74f97263ecbfd80d53701db9d554e948c1620c87
Edward2392 <66404622+Edward2392@users.noreply.github.com>
2021-05-13 14:59:50 +0300

    note prj

--- app/src/main/java/com/example/mynote/MainActivity.java
+++ app/src/main/java/com/example/mynote/MainActivity.java
@@ -1,23 +1,44 @@
 package com.example.mynote;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.ListFragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.mynote.domain.Record;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements FirstFragment.OnRecordClicked {
 
     private boolean isLandscape = false;
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         isLandscape = getResources().getBoolean(R.bool.isLandscape);
         if (!isLandscape) {
@@ -33,10 +54,63 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.OnR
         }
     }
 
+    private void initView() {
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
+    }
+
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (navigateFragment(id)) {
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.first_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.search) {
+            Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (item.getItemId() == R.id.action_one) {
+            Toast.makeText(this, "Добавить", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public void onRecordClicked(Record record) {
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
 
         if (isLandscape) {
             fragmentManager.beginTransaction()
@@ -49,5 +123,21 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.OnR
                     .commit();
         }
 
+    }
+
+    private boolean navigateFragment(int id) {
+
+        switch (id) {
+            case R.id.action_about:
+                addFragment(new AboutFragment());
+                return true;
+        }
+        return false;
+    }
+
+    private void addFragment(AboutFragment aboutFragment) {
+        fragmentManager.beginTransaction()
+                .replace(R.id.action_about, aboutFragment)
+                .commit();
     }
 }
